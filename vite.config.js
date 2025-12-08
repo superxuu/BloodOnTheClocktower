43ddd24d9b5a366b7041a99d4b17810aa2c5a254
Asa~ <583079759@qq.com>
2025-12-08 16:47:42 +0800

    Update vite.config.js configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/BloodOnTheClocktower/',  // Gitee Pages 仓库名
   server: {
     port: 8058,
     host: true
